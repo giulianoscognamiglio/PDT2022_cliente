0acff9466a17521151038260bbe308be1752c828
LucianoMosca <lmd62018@gmail.com>
2023-01-30 23:42:51 -0300

    Se modifica el AltaReclamo por los atributos agregados, se crea el PanelGestionJustificados y se agrega botón en PanelMenu

---
+++ src/GUI/PanelGestionJustificados.java
@@ -0,0 +1,253 @@
+package GUI;
+
+import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import com.exceptions.ServiciosException;
+import com.entities.*;
+
+import controlador.DAOGeneral;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PanelGestionJustificados extends JPanel {
+
+	private DefaultTableModel modeloTabla;
+
+	public PanelGestionJustificados() {
+
+		setBounds(0, 0, 684, 581);
+		setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(63, 152, 561, 238);
+		add(scrollPane);
+
+		modeloTabla = new DefaultTableModel(new Object[][] { { null, null}, },
+				new String[] { "ID", "Estudiante", "Estado"});
+		JTable table = new JTable();
+		table.setModel(modeloTabla);
+		scrollPane.setViewportView(table);
+		
+
+		JLabel lblJustificados = new JLabel("Justificados");
+		lblJustificados.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJustificados.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblJustificados.setBounds(235, 37, 198, 36);
+		add(lblJustificados);
+	}
+//		
+//		JButton btnBaja = new JButton("Baja");
+//		btnBaja.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//
+//				
+//				try {
+//					
+//					Reclamo reclamoDB = seleccionarReclamo(table);
+//
+//					System.out.println(reclamoDB);
+//					DAOGeneral.reclamoBean.borrar(reclamoDB.getId_reclamo());
+//					
+//						
+//					JOptionPane.showMessageDialog(null, "Reclamo dado de baja con éxito", null,
+//							JOptionPane.PLAIN_MESSAGE);
+//				} catch (ServiciosException e1) {
+//					e1.printStackTrace();
+//				}
+//				cargarTabla(DAOGeneral.reclamoBean.obtenerTodos());
+//			}
+//		});
+//		btnBaja.setBounds(206, 500, 85, 21);
+//		add(btnBaja);
+//		
+//		JButton btnDetalle = new JButton("Detalle");
+//		btnDetalle.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				
+//				
+//				try {
+//					
+//					
+//					Reclamo reclamoDB = seleccionarReclamo(table);
+//
+//					DetalleReclamo.reclamo=reclamoDB;
+//					
+//					DetalleReclamo detRecl = new DetalleReclamo();
+//					
+//					detRecl.setVisible(true);
+//
+//				} catch (ServiciosException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//				
+//			}
+//		});
+//		btnDetalle.setBounds(331, 500, 85, 21);
+//		add(btnDetalle);
+//		
+//		JComboBox comboBox = new JComboBox();
+//		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar", "En proceso", "Finalizado"}));
+//		comboBox.setBounds(496, 428, 128, 26);
+//		add(comboBox);
+//		
+//		JLabel lblEstado = new JLabel("Modificar estado");
+//		lblEstado.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblEstado.setBounds(496, 405, 128, 13);
+//		add(lblEstado);
+//		
+//		JButton btnActualizar = new JButton("Actualizar");
+//		btnActualizar.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				
+//				Reclamo reclamoDB = seleccionarReclamo(table);
+//
+//				String estado =  comboBox.getSelectedItem().toString().toUpperCase();
+//				try {
+//					
+//					modificarEstado(reclamoDB,estado);
+//					
+//					cargarTabla(DAOGeneral.reclamoBean.obtenerTodos());
+//
+//					
+//				} catch (ServiciosException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//				
+//			}
+//		});
+//		btnActualizar.setBounds(496, 479, 128, 21);
+//		add(btnActualizar);
+//		
+//		DefaultComboBoxModel modeloCombo = new DefaultComboBoxModel();
+//		JComboBox comboBoxEstado = new JComboBox();
+//		comboBoxEstado.setModel(modeloCombo);
+//
+//		modeloCombo.addElement("Todos");		
+//		modeloCombo.addElement("En proceso");
+//		modeloCombo.addElement("Finalizado");
+//		modeloCombo.addElement("Ingresado");
+
+//		comboBoxEstado.setBounds(235, 106, 198, 36);
+//		add(comboBoxEstado);
+//
+//		//Filtrar
+//		comboBoxEstado.addItemListener(new ItemListener() {
+//			@Override
+//			public void itemStateChanged(ItemEvent arg0) {
+//				try {
+//					//primero nos traemos todos los reclamos y luego creamos una lista auxiliar donde vamos a almacenar los
+//					//reclamos filtrados
+//					List<Reclamo> reclamos = DAOGeneral.reclamoBean.obtenerTodos();
+//					List<Reclamo> reclamosFiltrados;
+//
+//					if (comboBoxEstado.getSelectedItem().toString() == "En proceso") {
+//						
+//						//aca utilizamos .stream().filter().collect para filtrar los elementos de la lista auxiliar y retornamos
+//						reclamosFiltrados = reclamos.stream().filter(r -> r.getEstado().equals("EN PROCESO")).collect(Collectors.toList());
+//						cargarTabla(reclamosFiltrados);
+//
+//					} else if (comboBoxEstado.getSelectedItem().toString() == "Finalizado") {
+//						
+//						
+//						//aca utilizamos .stream().filter().collect para filtrar los elementos de la lista auxiliar y retornamos
+//							reclamosFiltrados = reclamos.stream().filter(r -> r.getEstado().equals("FINALIZADO")).collect(Collectors.toList());
+//							cargarTabla(reclamosFiltrados);
+//							
+//					} else if (comboBoxEstado.getSelectedItem().toString() == "Ingresado") {
+//						
+//						
+//						//aca utilizamos .stream().filter().collect para filtrar los elementos de la lista auxiliar y retornamos
+//						reclamosFiltrados = reclamos.stream().filter(r -> r.getEstado().equals("INGRESADO")).collect(Collectors.toList());
+//						cargarTabla(reclamosFiltrados);
+//					}  else if (comboBoxEstado.getSelectedItem().toString() == "Todos") {
+//												
+//						//aca retornamos todos los elementos de la lista
+//							cargarTabla(reclamos);
+//					} 	
+//					
+//
+//				} catch (Exception e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//		});
+//		
+//		cargarTabla(DAOGeneral.reclamoBean.obtenerTodos());
+//
+//	}
+//	
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////
+	public void cargarTabla(List<Justificado> justificados) {
+		modeloTabla.setRowCount(0);
+		for (Justificado j : justificados) {
+			
+					
+
+			try {
+				
+				Vector v = new Vector();
+				Usuario usuarioDB;
+				usuarioDB = DAOGeneral.usuarioBean.obtenerPorId(j.getEstudiante());
+				
+				v.addElement(j.getId_justificado());
+				v.addElement(usuarioDB.getNombre1() + " " + usuarioDB.getApellido1());
+
+				modeloTabla.addRow(v);
+
+
+			} catch (ServiciosException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+//	public void modificarEstado(Reclamo reclamoDB, String nuevoEstado) throws ServiciosException {
+//		//seteamos el estado nuevo
+//		
+//		reclamoDB.setEstado(nuevoEstado);
+//		
+//		//mandamos el registro modificado a la base
+//		DAOGeneral.reclamoBean.actualizar(reclamoDB);
+//		
+//	}
+//	
+//	public Reclamo seleccionarReclamo(JTable table) {
+//		//metodo para seleccionar un reclamo de la tabla
+//		int column = 0;
+//		int row = table.getSelectedRow();
+//		Long id = Long.parseLong(table.getModel().getValueAt(row, column).toString());
+//		
+//		Reclamo reclamoDB = DAOGeneral.reclamoBean.obtenerReclamo(id);
+//		
+//		return reclamoDB;
+//	}
+}
---
+++ src/GUI/PanelMenu.java
@@ -0,0 +1,389 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import com.entities.Usuario;
+import com.exceptions.ServiciosException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PanelMenu extends JPanel {
+
+	public static Usuario usuarioIngresado;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelMenu() {
+		analistaGUI();
+	}
+
+	public void estudianteGUI() {
+		removeAll();
+		setBounds(0, 0, 907, 581);
+
+		setLayout(null);
+
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(Color.LIGHT_GRAY);
+		panelMenu.setBounds(0, 0, 226, 581);
+		add(panelMenu);
+		panelMenu.setLayout(null);
+
+		JPanel panelDinamico = new JPanel();
+		panelDinamico.setBounds(223, 0, 684, 581);
+		add(panelDinamico);
+		panelDinamico.setLayout(null);
+
+		JLabel lblTipoUsuario = new JLabel("Estudiante");
+		lblTipoUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTipoUsuario.setBounds(54, 80, 121, 13);
+		panelMenu.add(lblTipoUsuario);
+
+		JButton btnAltaReclamos = new JButton("Alta Reclamo");
+		btnAltaReclamos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				AltaReclamo altaReclamo;
+				try {
+					altaReclamo = new AltaReclamo();
+					altaReclamo.setVisible(true);
+
+				} catch (ServiciosException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+		btnAltaReclamos.setBounds(48, 331, 127, 21);
+		panelMenu.add(btnAltaReclamos);
+
+		JButton btnFuncionalidades = new JButton("Funcionalidades");
+		btnFuncionalidades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				PanelFuncionalidades funcionalidades = new PanelFuncionalidades();
+				panelDinamico.removeAll();
+				funcionalidades.setSize(684, 581);
+				funcionalidades.setLocation(0, 0);
+				panelDinamico.add(funcionalidades);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnFuncionalidades.setBounds(48, 364, 127, 21);
+		panelMenu.add(btnFuncionalidades);
+
+		JButton btnReclamos = new JButton("Reclamos");
+		btnReclamos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				PanelGestionReclamosEstudiante gestionReclamos = new PanelGestionReclamosEstudiante();
+				panelDinamico.removeAll();
+				gestionReclamos.setSize(684, 581);
+				gestionReclamos.setLocation(0, 0);
+				panelDinamico.add(gestionReclamos);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnReclamos.setBounds(48, 395, 127, 21);
+		panelMenu.add(btnReclamos);
+
+		JButton btnEditarPerfil = new JButton("Editar Perfil");
+		btnEditarPerfil.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				ActualizarUsuario actUsuario = new ActualizarUsuario();
+				panelDinamico.removeAll();
+				actUsuario.setSize(684, 581);
+				actUsuario.setLocation(0, 0);
+				panelDinamico.add(actUsuario);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnEditarPerfil.setBounds(48, 428, 127, 21);
+		panelMenu.add(btnEditarPerfil);
+
+		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Login login = new Login();
+				MenuPrincipal.usuarioIngresado = null;
+				MenuPrincipal.getInstancia().setVisible(false);
+				login.setVisible(true);
+				
+			}
+		});
+		btnLogOut.setBounds(48, 475, 127, 21);
+		panelMenu.add(btnLogOut);
+
+		JLabel lblNombreUsuario = new JLabel("nombreUsuario");
+		lblNombreUsuario.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNombreUsuario.setBounds(54, 59, 121, 13);
+		lblNombreUsuario.setText(usuarioIngresado.getUsuario());
+		panelMenu.add(lblNombreUsuario);
+
+	}
+
+	public void analistaGUI() {
+		removeAll();
+		setBounds(0, 0, 907, 581);
+
+		setLayout(null);
+
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(Color.LIGHT_GRAY);
+		panelMenu.setBounds(0, 0, 226, 581);
+		add(panelMenu);
+		panelMenu.setLayout(null);
+
+		JPanel panelDinamico = new JPanel();
+		panelDinamico.setBounds(223, 0, 684, 581);
+		add(panelDinamico);
+		panelDinamico.setLayout(null);
+
+		JLabel lblTipoUsuario = new JLabel("Analista");
+		lblTipoUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTipoUsuario.setBounds(54, 80, 121, 13);
+		panelMenu.add(lblTipoUsuario);
+
+		JButton btnRoles = new JButton("Roles");
+		btnRoles.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				PanelFuncionalidadesAnalista funcionalidades = new PanelFuncionalidadesAnalista();
+				panelDinamico.removeAll();
+				funcionalidades.setSize(684, 581);
+				funcionalidades.setLocation(0, 0);
+				panelDinamico.add(funcionalidades);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnRoles.setBounds(23, 347, 170, 21);
+		panelMenu.add(btnRoles);
+
+		JButton btnEditarPerfil = new JButton("Editar Perfil");
+		btnEditarPerfil.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				ActualizarUsuario actUsuario = new ActualizarUsuario();
+				panelDinamico.removeAll();
+				actUsuario.setSize(684, 581);
+				actUsuario.setLocation(0, 0);
+				panelDinamico.add(actUsuario);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnEditarPerfil.setBounds(23, 380, 170, 21);
+		panelMenu.add(btnEditarPerfil);
+
+		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Login login = new Login();
+				MenuPrincipal.usuarioIngresado = null;
+				MenuPrincipal.getInstancia().setVisible(false);
+				login.setVisible(true);
+			}
+		});
+		btnLogOut.setBounds(23, 475, 170, 21);
+		panelMenu.add(btnLogOut);
+
+		JButton btnAltaUsuario = new JButton("Alta Usuario");
+		btnAltaUsuario.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				RegistroUsuario registro = new RegistroUsuario();
+				registro.setVisible(true);
+
+			}
+		});
+		btnAltaUsuario.setBounds(23, 280, 170, 21);
+		panelMenu.add(btnAltaUsuario);
+
+		JButton btnBajaUsuario = new JButton("Gestión de usuarios");
+		btnBajaUsuario.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				PanelBajaUsuario bajaUsuario = PanelBajaUsuario.getInstance();
+				panelDinamico.removeAll();
+				bajaUsuario.setSize(684, 581);
+				bajaUsuario.setLocation(0, 0);
+				panelDinamico.add(bajaUsuario);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+
+			}
+		});
+		btnBajaUsuario.setBounds(23, 313, 170, 21);
+		panelMenu.add(btnBajaUsuario);
+
+		JLabel lblNombreUsuario = new JLabel("nombreUsuario");
+		lblNombreUsuario.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNombreUsuario.setBounds(54, 57, 121, 13);
+
+		lblNombreUsuario.setText(usuarioIngresado.getUsuario());
+
+		panelMenu.add(lblNombreUsuario);
+
+		JButton btnReclamos = new JButton("Gestionar reclamos");
+		btnReclamos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				PanelGestionReclamos gestionReclamos = new PanelGestionReclamos();
+				panelDinamico.removeAll();
+				gestionReclamos.setSize(684, 581);
+				gestionReclamos.setLocation(0, 0);
+				panelDinamico.add(gestionReclamos);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+
+			}
+		});
+		btnReclamos.setBounds(23, 444, 170, 21);
+		panelMenu.add(btnReclamos);
+
+//
+//		---------------------EN OBRA!-----------------
+//		
+		
+		JButton btnJustificados = new JButton("Gestionar justificados de inasistencias");
+		btnJustificados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				PanelGestionJustificados gestionJustificados = new PanelGestionJustificados();
+				panelDinamico.removeAll();
+				gestionJustificados.setSize(684, 581);
+				gestionJustificados.setLocation(0, 0);
+				panelDinamico.add(gestionJustificados);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+
+			}
+		});
+		btnJustificados.setBounds(23, 412, 170, 21);
+		panelMenu.add(btnJustificados);
+		
+		JButton btnAnaliticaDeReportes = new JButton("Analitica de reportes");
+		btnAnaliticaDeReportes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				PanelAnaliticaReportes analiticaReportes = new PanelAnaliticaReportes();
+				panelDinamico.removeAll();
+				analiticaReportes.setSize(684, 581);
+				analiticaReportes.setLocation(0, 0);
+				panelDinamico.add(analiticaReportes);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnAnaliticaDeReportes.setBounds(23, 103, 170, 21);
+		panelMenu.add(btnAnaliticaDeReportes);
+
+	}
+
+	
+//	
+//	-----------------------------------------------------
+//	
+	
+	public void tutorGUI() {
+		removeAll();
+		setBounds(0, 0, 907, 581);
+
+		setLayout(null);
+
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(Color.LIGHT_GRAY);
+		panelMenu.setBounds(0, 0, 226, 581);
+		add(panelMenu);
+		panelMenu.setLayout(null);
+
+		JPanel panelDinamico = new JPanel();
+		panelDinamico.setBounds(223, 0, 684, 581);
+		add(panelDinamico);
+		panelDinamico.setLayout(null);
+
+		JLabel lblTipoUsuario = new JLabel("Tutor");
+		lblTipoUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTipoUsuario.setBounds(54, 80, 121, 13);
+		panelMenu.add(lblTipoUsuario);
+
+		JButton btnFuncionalidades = new JButton("Funcionalidades");
+		btnFuncionalidades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				PanelFuncionalidades funcionalidades = new PanelFuncionalidades();
+				panelDinamico.removeAll();
+				funcionalidades.setSize(684, 581);
+				funcionalidades.setLocation(0, 0);
+				panelDinamico.add(funcionalidades);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnFuncionalidades.setBounds(48, 364, 127, 21);
+		panelMenu.add(btnFuncionalidades);
+
+		JButton btnEditarPerfil = new JButton("Editar Perfil");
+		btnEditarPerfil.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				ActualizarUsuario actUsuario = new ActualizarUsuario();
+				panelDinamico.removeAll();
+				actUsuario.setSize(684, 581);
+				actUsuario.setLocation(0, 0);
+				panelDinamico.add(actUsuario);
+				panelDinamico.revalidate();
+				panelDinamico.repaint();
+			}
+		});
+		btnEditarPerfil.setBounds(48, 395, 127, 21);
+		panelMenu.add(btnEditarPerfil);
+
+		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Login login = new Login();
+				MenuPrincipal.usuarioIngresado = null;
+				MenuPrincipal.getInstancia().setVisible(false);
+				login.setVisible(true);
+			}
+		});
+		btnLogOut.setBounds(48, 475, 127, 21);
+		panelMenu.add(btnLogOut);
+
+		JLabel lblNombreUsuario = new JLabel("nombreUsuario");
+		lblNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNombreUsuario.setBounds(54, 57, 121, 13);
+		lblNombreUsuario.setText(usuarioIngresado.getUsuario());
+		panelMenu.add(lblNombreUsuario);
+	}
+}
